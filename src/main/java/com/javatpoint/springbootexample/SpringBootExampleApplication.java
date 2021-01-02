3caa6c17d5c43fc165f00e9334a32acbff632924
Sadiq92 <aqeelhashmi92@gmail.com>
2021-01-02 18:19:57 +0530

    second commit

--- src/main/java/com/javatpoint/springbootexample/SpringBootExampleApplication.java
+++ src/main/java/com/javatpoint/springbootexample/SpringBootExampleApplication.java
@@ -1,11 +1,24 @@
 package com.javatpoint.springbootexample;
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootExampleApplication 
 {
+	public static Logger logger = LoggerFactory.getLogger(SpringBootExampleApplication.class);
+	
+	@PostConstruct
+	public void init() {
+		logger.info("Application started...");
+	}
+	
 public static void main(String[] args) 
 {
+	logger.info("Application executed");
 SpringApplication.run(SpringBootExampleApplication.class, args);
 }
 }
